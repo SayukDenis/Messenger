858a0a1fb4b573aef265b4b4ea294fca95c3f05d
SayukDenis <chinazes.4822@gmail.com>
2023-09-05 18:53:35 +0300

    Configure Project

---
+++ Messenger/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react';
+export default function App() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
