62e006d81e32454a3fd860778ea0883cf0e88d5a
Viktor Hraboviuk <zatibmag25565@gmail.com>
2023-09-28 21:24:55 +0200

    add DBUser.tsx(in future will be with DB(rn const)

--- Messenger/Pages/Profiles/GroupProfile/EditGroup/EditGroup.tsx
+++ Messenger/Pages/Profiles/GroupProfile/EditGroup/EditGroup.tsx
@@ -1,14 +1,13 @@
-// Ваш файл GroupProfile.tsx
 import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native";
 import { styles } from "./ProfileGroupStyles"; // Адаптувати шлях до правильного файлу
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { GroupHead } from "./GroupHeadProfile";
 import { GroupBio } from "./GroupBio";
-import { User, UserProps } from "./UserProfile";
+import { UserProps, user } from "../../SemiComponents/DBUser";
 import { GroupName } from "./GroupNameInput";
 import { EditGroupPhoto } from "./EditGroupPhoto";
-import { UsersList } from "./UsersList";
+import { User } from "./UserProfile";
 
 export default function EditGroup() {
   const [users, setUsers] = useState<UserProps[]>([]);
@@ -20,30 +19,6 @@ export default function EditGroup() {
     "https://picsum.photos/id/237/536/354"
   );
 
-  const user: UserProps = {
-    ImagePath: "https://picsum.photos/id/237/536/354",
-    Nickname: "User",
-  };
-
-  useEffect(() => {
-    // Ваш код для отримання користувачів з бази даних.
-    // Поки дані не завантажені, встановлюємо тимчасове значення user
-    // Наприклад, якщо дані завантажуються асинхронно з використанням async/await:
-    async function fetchData() {
-      try {
-        // Використовуйте свій код для отримання користувачів тут
-        const response = await fetch("DB_URL");
-        const data = await response.json();
-        setUsers(data); // Оновлюємо users, коли дані завантажені
-      } catch (error) {
-        console.error("Помилка завантаження даних:", error);
-      }
-    }
-
-    // Викликаємо функцію для завантаження даних
-    fetchData();
-  }, []); // Передайте необхідні параметри для запиту до бази даних
-
   return (
     <GestureHandlerRootView style={styles.wrapper}>
       <SafeAreaView style={styles.container}>
@@ -51,11 +26,7 @@ export default function EditGroup() {
         <EditGroupPhoto />
         <GroupName setGroupNameExample={setGroupNameExample} />
         <GroupBio setBioTextExample={setBioTextExample} />
-        {users.length > 0 ? (
-          <UsersList users={users} />
-        ) : (
-          <UsersList users={[user]} /> // Відображаємо user, коли users порожній
-        )}
+        <User ImagePath={user.ImagePath} Nickname={user.Nickname} />
       </SafeAreaView>
     </GestureHandlerRootView>
   );
---
+++ Messenger/Pages/Profiles/SemiComponents/DBUser.tsx
@@ -0,0 +1,9 @@
+export interface UserProps {
+  ImagePath: string;
+  Nickname: string;
+}
+
+export const user: UserProps = {
+  ImagePath: "https://picsum.photos/id/1084/536/354",
+  Nickname: "Aboba ABOBA aboba ABOBA AboBA BaoBAB",
+};
