19794dfd56f31e08f402021cc7ed6ab7b0c63ad0
Vlad <Saphire.g8@gmail.com>
2023-10-29 20:01:27 +0100

    pull main

--- Messenger/App.tsx
+++ Messenger/App.tsx
@@ -3,32 +3,19 @@ import {
   View,
   StyleSheet,
   Text,
-  Dimensions,
-  StatusBar,
-  AppRegistry,
 } from "react-native";
-import ChatList from "./Pages/ChatList/ChatList";
-import Authorization from './Pages/Authorization/Authorization'; 
-import PhoneCodeRegistration from './Pages/Authorization/Pages/PhoneCodeRegistration';
-import Registration from './Pages/Authorization/Pages/Registration';
-import CodePassword from './Pages/CodePassword/CodePassword'; 
+
 import React from "react";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { rootReducer} from "./ReducersAndActions/Reducers/ChatListActions/ChatListActions";
+
 export default function App() {
-  StatusBar.setBarStyle("dark-content");
-  const store=createStore(rootReducer)
+ 
   return (
-    <Provider store={store}>
-      <View style={style.container}>
-        <ChatList />
+      <View>
+        <Text>Start working!</Text>
       </View>
-    </Provider>
   );
 }
 
-const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 const style = StyleSheet.create({
   container: {
     flex: 1,
@@ -37,5 +24,4 @@ const style = StyleSheet.create({
 });
 
 registerRootComponent(App);
-AppRegistry.registerComponent("10", () => App);
-AppRegistry.registerComponent("10".toLowerCase(), () => App);
+
