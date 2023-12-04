fdfadc15e5eb641876accb58d9b2dd6ae51570a0
SayukDenis <chinazes.4822@gmail.com>
2023-12-04 09:15:34 +0200

    Update Reducer

--- Messenger/ReducersAndActions/GeneralReducer/GeneralReducer.tsx
+++ Messenger/ReducersAndActions/GeneralReducer/GeneralReducer.tsx
@@ -1,9 +1,15 @@
 import { combineReducers } from "redux";
 import rootReducerChatList from "../Reducers/ChatListReducers/ChatListReducers";
+import ChatReducer from "../Reducers/ChatsActions/ChatsActions";
+import DialogueReducer from "../Reducers/ProfileActions/DialogueActions/DialogueActions";
+import SelfProfileReducer from "../Reducers/SelfProfileReducer/SelfProfileReducer";
 
 const rootReducer=combineReducers(
     {
         chatListReducer:rootReducerChatList,
+        ChatReducer:ChatReducer,
+        DialogueReducer:DialogueReducer,
+        SelfProfileReducer:SelfProfileReducer,
     }
 )
 export default rootReducer;
