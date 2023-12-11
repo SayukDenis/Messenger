43f0ac8bae195d4d0eaf85fed55ad0512bba9bba
Vlad <Saphire.g8@gmail.com>
2023-12-11 15:39:20 +0100

    feat: add initialization branch, update model "Branch"

--- Messenger/dao/Models/Chats/Branch.ts
+++ Messenger/dao/Models/Chats/Branch.ts
@@ -3,7 +3,12 @@ import Role from './Role';
 import Model from '../Model';
 
 export default class Branch extends Model {
+    constructor(title: string) {
+        super();
+        this.title = title;
+    }
     branchId?: number;
+    title!: string;
     messages: Array<Message> = new Array;
     internalBranches: Array<Branch> = new Array;
     pinnedMessage: Array<Message> = new Array;
@@ -15,6 +20,7 @@ export default class Branch extends Model {
         name: 'branchs',
         properties: {
             branchId: { type: 'integer', indexed: true },
+            title: 'text',
             messages: { type: 'list', objectType: Message },
             internalBranches: { type: 'list', objectType: Branch },
             pinnedMessage: { type: 'list', objectType: Message },
