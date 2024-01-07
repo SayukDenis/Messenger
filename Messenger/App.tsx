4d62d83e698f823e224efd6da94e643db92114a8
Vlad <Saphire.g8@gmail.com>
2024-01-07 18:08:29 +0100

    some fix +gitignore

--- Messenger/App.tsx
+++ Messenger/App.tsx
@@ -1,12 +1,21 @@
 import { registerRootComponent } from "expo";
-import { StatusBar, AppRegistry } from "react-native";
+import { StatusBar, AppRegistry, View } from "react-native";
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./ReducersAndActions/ConfigureStore/ConfigureStore";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./Navigation/Navigation";
+import { printSelfProfile } from './Initialization/Print';
+
+
+
 export default function App() {
   StatusBar.setBarStyle("dark-content");
+
+  printSelfProfile();
+
+  return (<View></View>)
+
   return (
     <SafeAreaProvider>
       <StatusBar translucent backgroundColor="transparent" />
--- Messenger/Initialization/Print.tsx
+++ Messenger/Initialization/Print.tsx
@@ -27,7 +27,7 @@ export function printFullData() {
     printObject(selfProfile, 0);
 }
 
-function printObject(obj, depth) {
+function printObject(obj: any, depth: any) {
     for (const key in obj) {
         if (obj.hasOwnProperty(key)) {
             const value = obj[key];
@@ -48,6 +48,6 @@ function printObject(obj, depth) {
     }
 }
 
-function getIndentation(depth) {
+function getIndentation(depth: any) {
     return '  '.repeat(depth);
 }
